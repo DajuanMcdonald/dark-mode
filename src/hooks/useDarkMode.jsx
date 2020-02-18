01bddf09e820d86250f0bbf65f3b19c7b08f2a71
fiopwk <flopwiki@gmail.com>
2020-02-18 11:50:18 -0500

    added dark-mode class to body

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,10 +1,16 @@
-import React, {useEffect} from 'react';
+import {useEffect} from 'react';
 import useLocalStorage from './useLocalStorage';
 
-export const useDarkMode = () => {
-   const [someValue, setSomeValue] = useLocalStorage('dots')
-};
+export const useDarkMode = (e) => {
+   const [darkMode, setDarkMode] = useLocalStorage('darkmode', e)
+
 
 useEffect(() => {
-    useLocalStorage ? console.log('dark-mode') : console.log('not-dark-mode')
-}, [ontoggle]);
+    darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
+}, [darkMode]);
+
+   return [darkMode, setDarkMode];
+
+};
+
+
