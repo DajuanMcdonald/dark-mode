6073e5c240a4dab28697fba4592c048b583fce24
fiopwk <flopwiki@gmail.com>
2020-02-18 11:52:35 -0500

    fixed useLocalStorage return setValue

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,4 +1,4 @@
-import  React, {useState} from 'react';
+import {useState} from 'react';
 export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
        const item = window.localStorage.getItem(key);
@@ -12,7 +12,7 @@ export const useLocalStorage = (key, initialValue) => {
 
 
 
-    return [storedValue];
+    return [storedValue, setValue];
 
 
 };
